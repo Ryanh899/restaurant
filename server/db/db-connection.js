9813e42c6396517d83de4b9b57270338a8902217
Ryan Hazzard <rhazzardphoto@gmail.com>
2020-09-04 11:20:21 -0700

    hid

--- server/db/db-connection.js
+++ server/db/db-connection.js
@@ -2,8 +2,6 @@
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true},  { useUnifiedTopology: true });
 
-// mongodb://ryan:root@ec2-3-86-7-121.compute-1.amazonaws.com:27017/test
-
 const db = mongoose.connection;
 
 db.on('error', err => {
